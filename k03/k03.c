4ceb8f0d7902fdc1ff7723761c59cddb62ed8dab
kondosaki <bb35319023@ms.nagasaki-u.ac.jp>
2020-07-03 16:23:46 +0900

    7/3 #1回目

--- k03/k03.c
+++ k03/k03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #ifdef CONST_SEED
 #define RAND_SEED 2020
@@ -19,10 +20,10 @@ int main(int argc, char* argv[])
     double sigma;
     int i;
     double dummy;
-
-
-
-
+    
+    sscanf(argv[1],"%lf",&mu);
+    sscanf(argv[2],"%lf",&sigma);
+    sscanf(argv[3],"%d",&num_dummy);
 
     printf("============================================\n");
     printf("template mean: %4.1lf\n",mu);
@@ -31,9 +32,9 @@ int main(int argc, char* argv[])
     printf("============================================\n");
 
     srand(RAND_SEED);
-    for(  ){
+    for(i=1 ; i<=num_dummy ; i++){
         /* r_stdnormを使って，1人のデータを捏造 */
-        dummy =   ;
+        dummy = sigma*r_stdnorm()+mu  ;
         printf("%5.2lf\n",dummy);
     }
 
